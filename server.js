02c433ea1a6c05bf27fa71195d664dcb64e48293
Tony Kung <lps.tony@gmail.com>
2016-10-05 17:22:42 -0500

    first commit

--- server.js
+++ server.js
@@ -2,4 +2,28 @@
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
 
-var jsonData = {count: 12, message: 'hey'};
+  var jsonData = {count: 12, message: 'hey'};
+  var fs = require('fs'); 
+  var express = require('express'); 
+  var app = express(); 
+
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html', function(err){
+      if(err){
+        res.status(500).send(err);  
+      }
+    })
+  });
+
+  app.get('/data', function(req, res){
+    res.json(jsonData);
+  });
+
+  var port = 4000;
+  app.listen(port, function(){
+    console.log('listen on localhost:', port);
+  });
+
+
+
+
